82111b44f64539a512da9f3f41f3bb22b7da130e
Lucas França <lucastjw5@gmail.com>
2021-05-28 20:53:22 -0400

    primeiro commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormacaoComponent } from './formacao/formacao.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { IdiomaComponent } from './idioma/idioma.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { HabilidadeComponent } from './habilidade/habilidade.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: InicioComponent},
+  {path:'formacao', component: FormacaoComponent},
+  {path:'Idioma', component: IdiomaComponent},
+  {path:'Experiencia', component: ExperienciaComponent},
+  {path:'Habilidade', component: HabilidadeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormacaoComponent } from './formacao/formacao.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { IdiomaComponent } from './idioma/idioma.component';
+import { HabilidadeComponent } from './habilidade/habilidade.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormacaoComponent,
+    ExperienciaComponent,
+    IdiomaComponent,
+    HabilidadeComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
